f3788f791104633ef4b50138041cafcf83c14327
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-05-25 14:00:23 +1000

    Exercise: Building a Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ListGroup from "./components/ListGroup";
 import React from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   let items = ["Sydney", "Tokyo", "Krabi", "Bali", "Bangkok", "Ho Chi Min"];
+  const textBtn = "Click Here!!!";
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
@@ -17,6 +19,11 @@ function App() {
       <Alert>
         Hello <i>Gorgeous</i>
       </Alert>
+      <Button
+        children={textBtn}
+        color="dark"
+        onClick={() => console.log("clicked")}
+      />
       <ListGroup
         items={items}
         heading="List Group"
